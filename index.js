a68480b4e21007f800043a05b569e77b01ac111f
ShanmaM <shanmathim38@gmail.com>
2025-02-03 10:49:05 +0530

    db config

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen('5000', ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://shanmathim38:9884417274@mycluster.0gvxm.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
